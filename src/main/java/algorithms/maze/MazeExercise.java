e9bdee314ddec3eb5e42bab413a9fe2107ed558f
Asger191 <ajunker@outlook.dk>
2026-04-27 16:22:45 +0200

    Solved backtracking portfolio

--- src/main/java/algorithms/maze/MazeExercise.java
+++ src/main/java/algorithms/maze/MazeExercise.java
@@ -7,7 +7,7 @@ public class MazeExercise {
             {1, 0, 1, 1},
             {1, 1, 1, 0},
             {0, 0, 1, 1},
-            {1, 1, 0, 1}
+            {1, 1, 0, 3}
     };
 
     static int[][] path = new int[N][N];
@@ -22,7 +22,31 @@ public class MazeExercise {
 
     // TODO: Implementer denne metode
     static boolean solveMaze(int row, int col) {
-        // ...
+
+            // Tjekke om du går udenfor labyrintens grænser
+            if(row > 3 || col > 3 || row < 0 || col < 0) return false;
+
+            // Har sat målet til 3 i maze og når vi har ramt det har vi ramt målet
+            if(maze[row][col] == 3){
+                path[row][col] = 1;
+                System.out.println("---------------------------------------------------------------------------------------------------------------------------------");
+                System.out.println("Juhuuu du har fundet målet");
+                return true; // Tjekke om feltet er gyldigt ([row][col] == 1)
+            }
+
+            if(maze[row][col] == 0) return false; // Tjekke om feltet er en væg
+
+            if(path[row][col] == 1) return false; // Tjekke om feltet er besøgt
+
+
+            path[row][col] = 1;
+
+        if(solveMaze(row + 1, col)) return true;
+        if(solveMaze(row - 1, col)) return true;
+        if(solveMaze(row, col + 1)) return true;
+        if(solveMaze(row, col - 1)) return true;
+
+        path[row][col] = 0;
         return false;
     }
 
